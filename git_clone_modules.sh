d9b31e50557ff7876686f7d816dc31f635cb0c75
python191 <python191@163.com>
2025-08-23 19:40:21 +0800

    git_clone_modules.sh modified

--- git_clone_modules.sh
+++ git_clone_modules.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
  
 
-git submodule add https://github.com/madler/zlib.git    3rd/zlib  -b v1.3.1
-if [ -f "./3rd/zlib/.gitmodules" ]; then
-    cd 3rd/zlib
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
+# git submodule add -f -b v1.3.1 https://github.com/madler/zlib.git    3rd/zlib   
+git clone  https://github.com/madler/zlib.git    3rd/zlib   
+ 
 
-git submodule add https://github.com/facebook/zstd.git  3rd/zstd  -b v1.5.7
-if [ -f "./3rd/zstd/.gitmodules" ]; then
-    cd 3rd/zstd
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
-
-git submodule add https://github.com/openssl/openssl.git 3rd/openssl -b openssl-3.5.2
-if [ -f "./3rd/openssl/.gitmodules" ]; then
-    cd 3rd/openssl
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
+git clone https://github.com/facebook/zstd.git  3rd/zstd  
+ 
+  
+git clone https://github.com/openssl/openssl.git   3rd/openssl   
+#  git clone --recursive https://github.com/openssl/openssl.git  3rd/openssl 
+# if [ -f "./3rd/openssl/.gitmodules" ]; then
+#     cd 3rd/openssl
+#     git submodule update --init --recursive --progress -v
+#     cd ../../
+# fi
 
 
-git submodule add https://github.com/rockdaboot/libpsl.git 3rd/libpsl -b libpsl-0.21.0
-if [ -f "./3rd/libpsl/.gitmodules" ]; then
-    cd 3rd/libpsl
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
+git clone  https://github.com/rockdaboot/libpsl.git  3rd/libpsl  
+ 
+
+git clone  https://github.com/curl/curl.git    3rd/curl   
+ 
+
+git clone  https://github.com/libjpeg-turbo/libjpeg-turbo.git  3rd/libjpeg-turbo  
 
 
-git submodule add https://github.com/curl/curl.git   3rd/curl -b curl-8_15_0
-if [ -f "./3rd/curl/.gitmodules" ]; then
-    cd 3rd/curl
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
 
-
-git submodule add https://github.com/libjpeg-turbo/libjpeg-turbo.git  3rd/libjpeg-turbo -b jpeg-9f
-if [ -f "./3rd/libjpeg-turbo/.gitmodules" ]; then
-    cd 3rd/libjpeg-turbo
-    git submodule update --init --recursive --progress -v
-    cd ../../
-fi
