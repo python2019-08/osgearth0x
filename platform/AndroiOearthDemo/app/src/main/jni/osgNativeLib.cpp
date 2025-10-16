eeafeb158e7a8d5bc36c69b948fc434f0a6841c6
python191 <python191@163.com>
2025-10-16 16:15:03 +0800

    platform/AndroiOearthDemo/app/src/main/jni/osgNativeLib.cpp  +   platform/AndroiOearthDemo/gradle/wrapper/gradle-wrapper.properties

--- platform/AndroiOearthDemo/app/src/main/jni/osgNativeLib.cpp
+++ platform/AndroiOearthDemo/app/src/main/jni/osgNativeLib.cpp
@@ -9,38 +9,38 @@
 OsgMainApp mainApp;
 
 extern "C" {
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_init(JNIEnv * env, jobject obj, jint width, jint height);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_step(JNIEnv * env, jobject obj);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseButtonPressEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseButtonReleaseEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseMoveEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_keyboardDown(JNIEnv * env, jobject obj, jint key);
-    JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_keyboardUp(JNIEnv * env, jobject obj, jint key);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_init(JNIEnv * env, jobject obj, jint width, jint height);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_step(JNIEnv * env, jobject obj);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseButtonPressEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseButtonReleaseEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseMoveEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_keyboardDown(JNIEnv * env, jobject obj, jint key);
+    JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_keyboardUp(JNIEnv * env, jobject obj, jint key);
 };
 
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_init(JNIEnv * env, jobject obj, jint width, jint height){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_init(JNIEnv * env, jobject obj, jint width, jint height){
     mainApp.initOsgWindow(0,0,width,height);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_step(JNIEnv * env, jobject obj){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_step(JNIEnv * env, jobject obj){
     mainApp.draw();
 }
 
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseButtonPressEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseButtonPressEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button){
     mainApp.mouseButtonPressEvent(x,y,button);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseButtonReleaseEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseButtonReleaseEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y, jint button){
     mainApp.mouseButtonReleaseEvent(x,y,button);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_mouseMoveEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_mouseMoveEvent(JNIEnv * env, jobject obj, jfloat x, jfloat y){
     mainApp.mouseMoveEvent(x,y);
 }
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_keyboardDown(JNIEnv * env, jobject obj, jint key){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_keyboardDown(JNIEnv * env, jobject obj, jint key){
     mainApp.keyboardDown(key);
 }
-extern "C" JNIEXPORT void JNICALL Java_osg_AndroidExample_osgNativeLib_keyboardUp(JNIEnv * env, jobject obj, jint key){
+extern "C" JNIEXPORT void JNICALL Java_com_example_androioearthdemo_osgNativeLib_keyboardUp(JNIEnv * env, jobject obj, jint key){
     mainApp.keyboardUp(key);
 }
 
